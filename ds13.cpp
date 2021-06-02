094cddca2a65618a8f195bc8bfd9bf4544f864bc
Jayant <jayantawasthi5@gmail.com>
2021-06-02 23:21:11 +0530

    13

---
+++ ds13.cpp
@@ -0,0 +1,34 @@
+int max=0;
+    int cur=0;
+    int l=0;
+    for(int i=0;i<n;i++)
+    {    
+        if(arr[i]<0)
+        {
+            l++;
+        }
+        cur=cur+arr[i];
+        if(cur>max)
+        {
+            max=cur;
+        }
+        if(cur<0)
+        {
+            cur=0;
+        }
+    }
+    if(l==n)
+    {
+        max=arr[0];
+        cur=arr[0];
+        for(int j=1;j<n;j++)
+        { 
+        cur=cur+arr[j];
+        if(cur>max)
+        {
+            max=cur;
+        }
+            
+        }
+    }
+    return max;
