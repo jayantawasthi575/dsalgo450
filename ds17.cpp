bbf73810e5da5ca767b3a3b4024a13e955f9964a
Jayant <jayantawasthi5@gmail.com>
2021-06-04 01:03:08 +0530

    17

--- ds17.cpp
+++ ds17.cpp
@@ -27,3 +27,28 @@ public:
         return maxi;
     }
 };
+
+
+time-O(N) 
+space-O(1)
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=prices[0];
+        int max=0;
+        for(int i=1;i<prices.size();i++)
+        {
+            if(min>prices[i])
+            {
+                min=prices[i];
+            }
+            if(prices[i]-min>max)
+            {
+                max=prices[i]-min;
+            }
+        }
+        return max;
+    }
+};
