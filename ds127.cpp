12326d617148d3b7b442e46e81c1a550b4dc13d6
Jayant <jayantawasthi5@gmail.com>
2021-06-20 08:47:51 +0530

    Reverse a linked list

--- ds127.cpp
+++ ds127.cpp
@@ -1,50 +1,110 @@
-class Solution
-{
-    public:
-    struct node *reverse (struct node *head, int k)
-    { 
-        int m=0;
-        node* current=head;
-        node* prev=NULL,*next=NULL;
-        node* temp,*hold;
-        int flag=0;
-        while(current!=NULL)
-        {   
-  
-            next = current->next;
-            current->next = prev;
-            prev = current;
-            current = next;
-            m++;
-            if(flag==0)
-            {
-                temp=prev;
-            }
-            if(current==NULL&&m!=k)
-            {
-                head->next=prev;
-            }
-            if(m==k)
-            {   
-                if(flag==0)
-                {temp=prev;
-                hold=current;
-                prev=NULL;
-                next=NULL;
-                 flag=1;
-                 m=0;
-                }
-                else{
-                   head->next=prev;
-                   head=hold;
-                   prev=NULL;
-                   next=NULL;
-                   m=0;
-                   hold=current;
-                }
-                m=0;
-            }
-        }
-        head=temp;
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+    Node(int data)
+    {
+        this->data=data;
+        next=NULL;
     }
 };
+//Iterative
+// struct LinkedList{
+//     Node* head;
+//     LinkedList(){ head=NULL; }
+//     void reverse()
+//     {
+//         Node* current=head;
+//         Node* prev=NULL,*next=NULL;
+//         while(current!=NULL)
+//         {
+//             next=current->next;
+//             current->next=prev;
+//             prev=current;
+//             current=next;
+//         }
+//         head=prev;
+//     }
+//     void print(){
+//         struct Node* temp=head;
+//         while(temp!=NULL)
+//         {
+//             cout<<temp->data<<" ";
+//             temp=temp->next;
+//         }
+//     }
+//     void push(int data)
+//     {
+//         Node* temp=new Node(data);
+//         temp->next=head;
+//         head=temp;        
+//     }
+// };
+
+
+// int main()
+// {
+//     LinkedList ll;
+//     ll.push(20);
+//     ll.push(4);
+//     ll.push(15);
+//     ll.push(85);
+//     cout<<"Given Linked List \n";
+//     ll.print();
+//     ll.reverse();
+//     cout<<"\n Reverse Linked List \n";
+//     ll.print();
+//     return 0;
+// }
+
+
+//Recursive
+
+struct LinkedList{
+    Node* head;
+    LinkedList(){
+        head=NULL;
+    }
+    Node* reverse(Node* head)
+    {
+        if(head==NULL || head->next==NULL)
+        return head;
+        Node* rest=reverse(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return rest;
+    }
+    void print()
+    {
+        struct Node* temp=head;
+        while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    }
+    void push(int data)
+    {
+        Node* temp=new Node(data);
+        temp->next=head;
+        head=temp;
+    }
+    
+};
+
+int main()
+{
+    LinkedList ll;
+    ll.push(20);
+    ll.push(4);
+    ll.push(15);
+    ll.push(85);
+    cout<<"Given Linked List \n";
+    ll.print();
+    ll.head=ll.reverse(ll.head);
+    cout<<"\nReverse Linked List \n";
+    ll.print();
+    return 0;
+}
