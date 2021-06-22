17eee6b34b9444f1f6e3a44fea422f299d3fc2de
Jayant <jayantawasthi5@gmail.com>
2021-06-22 09:44:08 +0530

    Check If Circular Linked List

---
+++ ds142.cpp
@@ -0,0 +1,13 @@
+bool isCircular(Node *head)
+{
+   Node* slow=head;
+   Node* fast=head;
+   while(fast!=NULL && fast->next!=NULL)
+   {
+       slow=slow->next;
+       fast=fast->next->next;
+       if(fast==slow)
+       return true;
+   }
+   return false;
+}
