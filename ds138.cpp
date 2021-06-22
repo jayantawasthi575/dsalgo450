f09ec0880bd1568023d66d6ce5c30db38c07d64c
Jayant <jayantawasthi5@gmail.com>
2021-06-22 07:24:09 +0530

    Intersection Point in Y shapped linked lists

---
+++ ds138.cpp
@@ -0,0 +1,17 @@
+ unordered_map<Node*,int>check;
+ int flag=-1;
+ while(head1!=NULL)
+ {
+     check[head1]=1;
+     head1=head1->next;
+ }
+ while(head2!=NULL)
+ {
+     if(check.find(head2)!=check.end())
+     {
+         flag=head2->data;
+         break;
+     }
+     head2=head2->next;
+ }
+ return flag;
