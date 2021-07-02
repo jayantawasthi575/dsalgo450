704837c78792638a0afd63a9a9c193e6530280b1
Jayant <jayantawasthi5@gmail.com>
2021-07-02 23:51:19 +0530

    static void insert_at_bottom(char x){

---
+++ ds284.cpp
@@ -0,0 +1,12 @@
+void insert_at_bottom(char x){
+
+    if(st.isEmpty())
+        st.push(x);
+
+    else{
+        char a = st.peek();
+        st.pop();
+        insert_at_bottom(x);
+        st.push(a);
+    }
+}
