f97122a4b27c3a0bf51f913cbdd8f4fbe9fefecd
Jayant <jayantawasthi5@gmail.com>
2021-07-04 19:21:05 +0530

    Interleave the first half of the queue with second half

---
+++ ds299.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+void interLeaveQueue(queue<int>& q)
+{
+    if(q.size()%2!=0)
+    cout<<"Input even numbers of integers"<<endl;
+    stack<int>s;
+    int halfSize=q.size()/2;
+    for(int i=0;i<halfSize;i++)
+    {
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+    for(int i=0;i<halfSize;i++)
+    {
+        q.push(q.front());
+        q.pop();
+    }
+    for (int i = 0; i < halfSize; i++) {
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+        q.push(q.front());
+        q.pop();
+    }
+}
+int main()
+{
+    queue<int> q;
+    q.push(11);
+    q.push(12);
+    q.push(13);
+    q.push(14);
+    q.push(15);
+    q.push(16);
+    q.push(17);
+    q.push(18);
+    q.push(19);
+    q.push(20);
+    interLeaveQueue(q);
+    int length = q.size();
+    for (int i = 0; i < length; i++) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    return 0;
+}
