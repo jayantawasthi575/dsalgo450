a296431afee75e17183364156cc69beb375b2a65
Jayant <jayantawasthi5@gmail.com>
2021-07-26 19:47:18 +0530

    Print all possible paths from top left to bottom right of a mXn matrix

---
+++ ds259.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printVector(vector<int> &v){
+	for(auto it:v){
+		cout<<it<<" ";
+	}
+	cout<<"\n";
+}
+void printPaths(int mat[][3], vector<int> v, int i, int j, int n, int m){
+	if(i > n || j > m) return;
+	v.push_back(mat[i][j]);
+	if(i==n && j==m){
+		printVector(v);
+	}
+	printPaths(mat,v,i+1,j,n,m);
+	printPaths(mat,v,i,j+1,n,m);
+	v.pop_back();
+}
+void printPathUtil(int mat[][3], int n, int m){
+	vector<int> v;
+	printPaths(mat,v,0,0,n,m);
+}
+int main(){
+	int mat[3][3]={
+		{1,2,3},
+		{4,5,6}
+	};
+	int n=2,m=3;
+	printPathUtil(mat,n-1,m-1);
+	return 0;
+}
