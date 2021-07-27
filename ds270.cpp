992fb3340a93eadc7e4d0bb9470a4a10b2938799
Jayant <jayantawasthi5@gmail.com>
2021-07-27 20:51:55 +0530

    Merge K sorted linked lists

---
+++ ds270.cpp
@@ -0,0 +1,31 @@
+struct compare {
+    bool operator()(
+        struct Node* a, struct Node* b)
+    {
+        return a->data > b->data;
+    }
+};
+class Solution{
+  public:
+    //Function to merge K sorted linked list.
+    Node * mergeKLists(Node *arr[], int K)
+    {
+         priority_queue<Node*,vector<Node*>,compare> pq;
+         for (int i = 0; i < K; i++)
+         if (arr[i] != NULL)
+             pq.push(arr[i]);
+         if (pq.empty())
+           return NULL;
+         Node* dummy=new Node(0);
+         Node* last=dummy;
+         while (!pq.empty()) {
+            Node* curr=pq.top();
+            pq.pop();
+            last->next=curr;
+            last=last->next;
+            if(curr->next!=NULL)
+            pq.push(curr->next);
+         }
+         return dummy->next;
+    }
+};
