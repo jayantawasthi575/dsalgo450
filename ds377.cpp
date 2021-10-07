1fca0345e104b2d4cdc9cda1665ad09c38937a41
Jayant <jayantawasthi5@gmail.com>
2021-10-07 20:31:29 +0530

    Longest Increasing Subsequence

---
+++ ds377.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
+       int lis[n];
+       lis[0]=1;
+       for(int i=1;i<n;i++)
+       {
+           lis[i]=1;
+           for(int j=0;j<i;j++)
+           {
+               if(a[i]>a[j] && lis[i]<lis[j]+1)
+                lis[i]=lis[j]+1;
+           }
+       }
+       return *max_element(lis, lis+n); 
+    }
+};
